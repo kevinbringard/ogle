de1e7ecb20c09e7eaef5247c7786974f266c10a7
Kevin Bringard <kevin.bringard@attinteractive.com>
2011-04-07 10:10:50 -0600

    Created a testing structure and placeholder files

--- lib/ogle/resource.rb
+++ lib/ogle/resource.rb
@@ -4,5 +4,19 @@ module Ogle
       @connection = connection
     end
 
+    # Get a listing of all the images Glance knows about
+    def all
+      @connection.get "/images"
+    end
+
+    # Get a detailed listing on all public VM images
+    def details
+      @connection.get "/images/detail"
+    end
+
+    # Get detailed meta-data on a specific image
+    def find image_id
+      @connection.head "/images/#{image_id}"
+    end
   end
 end
