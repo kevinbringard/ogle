9a8140737eb78ed29f2e5caca2def796d396ba55
retr0h <john@dewey.ws>
2011-06-07 23:23:26 -0700

    Set vcr to :none

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -35,7 +35,7 @@ end
 VCR.config do |c|
   c.stub_with :webmock
   c.cassette_library_dir     = "test/cassettes"
-  c.default_cassette_options = { :record => :new_episodes }
+  c.default_cassette_options = { :record => :none }
 end
 
 MiniTest::Unit.autorun
